60dc75e0c388741b786a2360b8baccdc39c3c187
Felix Godfree <felixfgodfree@gmail.com>
2018-08-29 15:26:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector) {
+  return document.getElementById('app').querySelector(selector);
+}
 
+function nestedTarget() {
+  return getFirstSelector('div div div div div');
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString();
+  }
+}
+
+function deepestChild() {
+  return document.getElementById('app').querySelectorAll('div div div div div')[1];
+}
