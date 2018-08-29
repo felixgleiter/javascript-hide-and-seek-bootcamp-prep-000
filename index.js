844f8298d13bb6a52e4137d9e991615c99a48b2d
Felix Godfree <felixfgodfree@gmail.com>
2018-08-29 15:00:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function increaseRankBy(n) {
   const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
   for (let i = 0; i < lis.length; i++) {
     var int = parseInt(lis[i].innerHTML, 10);
-    int = int - n;
+    int += n;
   }
   return lis;
 }
