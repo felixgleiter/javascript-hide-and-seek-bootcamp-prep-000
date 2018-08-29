e664eb3f71bf944a465a250bc42fc35129bf633b
Felix Godfree <felixfgodfree@gmail.com>
2018-08-29 14:20:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  return document.getElementById('app').querySelector(selector);
+}
 
+function nestedTarget() {
+  return getFirstSelector('div div div div div');
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < lis.length; i++) {
+    var int = parseInt(lis[i].innerHTML, 10);
+    int += n;
+  }
+}
+
+function deepestChild() {
+  return document.getElementById('app').querySelectorAll('div div div div div')[1];
+}
